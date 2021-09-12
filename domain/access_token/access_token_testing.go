218a1b3bd508f5fd4c9709ee01ba29b6c6eeb78c
Nicole Wert <nicalexxandra@gmail.com>
2021-09-12 21:09:49 +0900

    #9 test: Test GetNewAccessToken function

--- domain/access_token/access_token_testing.go
+++ domain/access_token/access_token_testing.go
@@ -1,3 +1,5 @@
+package access_token
+
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
@@ -5,5 +7,12 @@ import (
 
 func TestAccessTokenConstants(t *testing.T) {
 	assert.EqualValues(t, 24, expirationHours, "expiration time should be 24 hours")
+}
+
+func TestGetNewAccessToken(t *testing.T) {
+	at := GetNewAccessToken()
+	assert.False(t, at.IsExpired(), "brand new access token should not be expired")
+	assert.EqualValues(t, "", at.AccessToken, "new access")
+	assert.True(t, at.UserId == 0, "new access token should not have an associated user id")
 }
 
