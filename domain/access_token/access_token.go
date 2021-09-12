c2402fb809801b96f9ce79164d04d221362fc039
xyzhg <xy@xyzhg.com>
2021-09-12 19:32:54 +0900

    #3 feat: Check if access token is expired

--- domain/access_token/access_token.go
+++ domain/access_token/access_token.go
@@ -1,7 +1,13 @@
 package access_token
 
+import "time"
+
 type AccessToken struct {
 	AccessToken     string `json:"access_token"`
 	UserId          int64  `json:"user_id"`
 	TokenExpiration int64  `json:"token_expiration"`
+}
+
+func (at AccessToken) IsExpired() bool {
+	return time.Unix(at.TokenExpiration, 0).Before(time.Now().UTC())
 }
