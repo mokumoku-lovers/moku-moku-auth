bd7b1141dfd291948d2afea65920bca49f240815
xyzhg <xy@xyzhg.com>
2021-09-12 19:25:43 +0900

    #2 feat: Get new access token function

--- domain/access_token/access_token.go
+++ domain/access_token/access_token.go
@@ -1,5 +1,7 @@
 package access_token
 
+import "time"
+
 const (
 	expirationHours = 24
 )
@@ -8,4 +10,10 @@ type AccessToken struct {
 	AccessToken     string `json:"access_token"`
 	UserId          int64  `json:"user_id"`
 	TokenExpiration int64  `json:"token_expiration"`
+}
+
+func GetNewAccessToken() AccessToken {
+	return AccessToken{
+		TokenExpiration: time.Now().UTC().Add(expirationHours * time.Hour).Unix(),
+	}
 }
