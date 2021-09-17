73347b75053bf80a8d00b5b677957225a1914417
JiahuiChen99 <Jiahui1@hotmail.es>
2021-09-17 17:49:41 +0200

    feat: Start App - Application - App port - GetByID endpoint

---
+++ app/application.go
@@ -0,0 +1,20 @@
+package app
+
+import (
+	"github.com/gin-gonic/gin"
+	"moku-moku/domain/access_token"
+	"moku-moku/http"
+	"moku-moku/repository/db"
+)
+
+var (
+	router = gin.Default()
+)
+
+func StartApplication() {
+	atHandler := http.NewHandler(access_token.NewService(db.NewRepository()))
+
+	router.GET("/oauth/access_token/:access_token_id", atHandler.GetByID)
+
+	router.Run(":9001")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "moku-moku/app"
+
+func main() {
+	app.StartApplication()
+}
