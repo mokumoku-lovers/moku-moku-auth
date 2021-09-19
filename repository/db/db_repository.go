6c492220355797e75c233fece7c4552b7f405e6c
Nicole Wert <nicalexxandra@gmail.com>
2021-09-19 15:55:04 +0900

    #22 feature: query and update expiration in cassandra db

--- repository/db/db_repository.go
+++ repository/db/db_repository.go
@@ -24,3 +24,20 @@ func NewRepository() DBRepository {
 func (r *dbRespository) GetByID(id string) (*access_token.AccessToken, *errors.RestErr) {
 	return nil, nil
 }
+
+// Update expiration in db
+func (r *dbRespository) UpdateExpiration(at access_token.AccessToken) *errors.RestErr {
+	session, err := cassandra.GetSession()
+	if err != nil {
+		return errors.InternalServerError(err.Error())
+	}
+	defer session.Close()
+
+	if err := session.Query(queryUpdateExpiration,
+		at.AccessToken,
+		at.TokenExpiration,
+	).Exec(); err != nil {
+		return errors.InternalServerError(err.Error())
+	}
+	return nil
+}
