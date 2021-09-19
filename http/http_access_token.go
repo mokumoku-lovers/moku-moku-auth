78e6c312f7a4b6f9423dfc2b4cb4bc5e446f719a
xyzhg <xy@xyzhg.com>
2021-09-19 15:13:50 +0900

    feature: access token handler Create function and interface

--- http/http_access_token.go
+++ http/http_access_token.go
@@ -3,11 +3,13 @@ package http
 import (
 	"github.com/gin-gonic/gin"
 	"moku-moku/domain/access_token"
+	"moku-moku/utils/errors"
 	"net/http"
 )
 
 type AccessTokenHandler interface {
 	GetByID(*gin.Context)
+	Create(*gin.Context)
 }
 
 type accessTokenHandler struct {
@@ -22,4 +24,18 @@ func NewHandler(service access_token.Service) AccessTokenHandler {
 
 func (h *accessTokenHandler) GetByID(c *gin.Context) {
 	c.JSON(http.StatusNotImplemented, "To be implemented")
+}
+
+func (h *accessTokenHandler) Create(c *gin.Context) {
+	var at access_token.AccessToken
+	if err := c.ShouldBindJSON(&at); err != nil {
+		restErr := errors.BadRequest("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+	if err := h.service.Create(at); err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+	c.JSON(http.StatusCreated, at)
 }
