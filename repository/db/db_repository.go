2fca388c0e96440f626a190c132d1b7b3a527903
xyzhg <xy@xyzhg.com>
2021-09-19 15:08:59 +0900

    feature: db repository Create function and interface

--- repository/db/db_repository.go
+++ repository/db/db_repository.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"moku-moku/clients/cassandra"
 	"moku-moku/domain/access_token"
 	"moku-moku/utils/errors"
 )
@@ -11,6 +12,7 @@ const (
 
 type DBRepository interface {
 	GetByID(string) (*access_token.AccessToken, *errors.RestErr)
+	Create(token access_token.AccessToken) *errors.RestErr
 }
 
 type dbRespository struct {
@@ -22,4 +24,17 @@ func NewRepository() DBRepository {
 
 func (r *dbRespository) GetByID(id string) (*access_token.AccessToken, *errors.RestErr) {
 	return nil, nil
+}
+
+func (r *dbRespository) Create(at access_token.AccessToken) *errors.RestErr {
+	session, err := cassandra.GetSession()
+	if err != nil {
+		return errors.InternalServerError(err.Error())
+	}
+	defer session.Close()
+
+	if err := session.Query(queryCreateAccessToken, at.AccessToken, at.UserId, at.TokenExpiration).Exec(); err != nil {
+		return errors.InternalServerError(err.Error())
+	}
+	return nil
 }
